35a48dd37133f7ce4ebd038a45832a714fe7f778
Christopher Augg <chrisa@christopheraugg.com>
2015-10-01 15:59:05 -0700

    Added a second ViewController and created a modal segue from a button.

---
+++ Algorythm/PlaylistDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlaylistDetailViewController.swift
+//  Algorythm
+//
+//  Created by teamtreehouse.com as part of their iOS/Android tutorial
+//  modified by Chris Augg on 10/1/15.
+
+import UIKit
+
+class PlaylistDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
